87116e7e6a2d2ad881b804aa15dc6d1d60745aa5
unknown <zhibekchach@gmail.com>
2020-08-23 21:03:51 -0500

    DDT

---
+++ src/Test/Test_TC001_Login.py
@@ -0,0 +1,13 @@
+import unittest
+from time import sleep
+from src.BaseClass import EnvironmentSetup
+from src.Utility import *
+
+
+class TC001_login(EnvironmentSetup):
+
+    def test_login(self): ...
+
+
+if __name__ == '__main__':
+    unittest.main()
